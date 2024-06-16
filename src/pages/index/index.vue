d0d077ef812f067ffc20a76da5139cdbbbd2447d
Jc <840031139@qq.com>
2024-06-16 20:04:04 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <scroll-view class="index" :scroll-y="true" :refresher-enabled="a">
+    <view class="inner"></view>
+    <view class="inner"></view>
+    <view class="inner"></view>
+  </scroll-view>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import './index.scss'
+
+const a = ref(false)
+</script>
